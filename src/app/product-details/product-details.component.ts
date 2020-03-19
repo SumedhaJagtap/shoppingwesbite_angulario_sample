3508aada3ca60aad0fc226400e407d31e334532d
SumedhaJagtap <sumedhajagtap@gmail.com>
2020-03-19 20:54:47 +0530

    Services #1

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {products} from '../products';
+import {CartService} from '../cart.service';
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -11,6 +13,7 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private route:ActivatedRoute,
+    private cartService:CartService
   ) { }
 
   ngOnInit() {
@@ -19,4 +22,8 @@ export class ProductDetailsComponent implements OnInit {
     })
   }
 
+  addToCart(product){
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
 }
